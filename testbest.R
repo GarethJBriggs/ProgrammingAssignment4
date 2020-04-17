b1a16bfadd344d45b254e62abb69be931bb0d7ff
Gareth Briggs <garethjamesbriggs@gmail.com>
2020-04-17 12:43:14 +0100

    Initial comments
    Initial comments for best function of ex. 2 in R Rrogramming WK 4

---
+++ testbest.R
@@ -0,0 +1,29 @@
+##set directory
+
+##read data
+
+##set the outcomes terms in a vector, names of out come equalling column index for ease of subsetting
+
+##subset intial dataframe so that it contains only hospital, states and outcome coluns
+
+##rename the dataframe columns for ease of use
+
+##logically test if state argument value is contained in the dataframe subset of the state column. If false...
+
+##return error message of 'invalid state'
+
+##else logically test if outcome argumant value matches any of the outcomes of interest. If false...
+
+##return an error message of 'invalid outcome'
+
+##subset dataframe by state of interest' in states column
+
+##create logical vector checking for NA values
+
+##subset dataframe by logicical vector
+
+##rank the hospitals in the state by outcome
+
+##index the data to return the the hospital value with best outcome that is first alphabetically
+
+##return the hospital name
